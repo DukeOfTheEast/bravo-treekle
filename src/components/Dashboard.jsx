01068d50a6ed2cb1cd547bcf38d07bd3592ab1b8
Ukachukwu Ebuka <fralexander1999@gmail.com>
2023-10-25 14:42:44 +0100

    added dashboard balance

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import "../../src/App.css";
+import Andre from "../images/1.jpg";
+import Alert from "../assets/asset1.svg";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="dashboard">
+      <div className="dash-top">
+        <div className="dashtop1">
+          <img src={Andre} alt="" className="andre" />
+          <div>
+            <p className="well">Welcome</p>
+            <p>DeAndre</p>
+          </div>
+        </div>
+        <img src={Alert} alt="" className="alert" />
+      </div>
+
+      <div className="over-balance">
+        <div className="tot-tra">
+          <p>Total Available Balance</p>
+          <p>Transaction History</p>
+        </div>
+        <h3 className="money">N257,940</h3>
+        <div className="stars">
+          <div className="star1">
+            <p>*************</p>
+            <p>Commission Bal</p>
+          </div>
+          <div className="out-bal">
+            <div>
+              <p>*************</p>
+              <p>Outstanding Bal</p>
+            </div>
+            <p className="arrow">&raquo;</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
